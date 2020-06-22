d19938d2d3b82b9b882d7e03d5a6473576535d1a
jazz dhami <me@jazzdhami.com>
2020-06-22 11:03:21 +0530

    my colorflipper website..

---
+++ index.js
@@ -0,0 +1,15 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function() {
+  const randomNumber = getRandomnumber();
+  // console.log(randomNumber);
+
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomnumber() {
+  return Math.floor(Math.random() * colors.length);
+}
